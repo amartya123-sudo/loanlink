5132ad9c2b73b8e0ca9746479bd779f228431099
Ambud Lahiri <ambudlahiri123@gmail.com>
2023-04-25 09:30:24 +0530

    Dashboard Updated

--- lib/r_widgets/r_widgets.dart
+++ lib/r_widgets/r_widgets.dart
@@ -24,6 +24,121 @@ class CustomButton extends StatelessWidget {
   }
 }
 
+
+AssetImage riconimage(String imageName) {
+  return AssetImage(imageName);
+}
+Widget rtiles(BuildContext context,String text,String imageName){
+  return Center(
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+      child: SizedBox(
+        width: 160.0,
+        height: 100.0,
+        child: ListTile(
+          title: Text(text),
+          contentPadding: EdgeInsets.fromLTRB(0.0, 20.0, 20.0, 0.0),
+          onTap: (){
+            print("Hello World");
+          },
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(backgroundImage: riconimage(imageName)),
+          ),
+          // trailing: Icon(Icons.navigate_next),
+          iconColor: Colors.white,
+          textColor: Colors.white,
+          tileColor: Colors.black,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+        ),
+      ),
+    ),
+  );
+}
+Widget rtcredit(BuildContext context,String text){
+  return Center(
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+
+      child: ListTile(
+        title: Text(text),
+        contentPadding: EdgeInsets.fromLTRB(20.0, 6.0, 30.0, 10.0),
+        onTap: (){
+          print("Hello World");
+        },
+        leading: Icon(Icons.credit_card),
+        trailing: Icon(Icons.navigate_next),
+        iconColor: Colors.white,
+        textColor: Colors.white,
+        tileColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(90.0)),
+      ),
+    ),
+
+  );
+}
+Widget rtcal(BuildContext context,String text,String imageName){
+  return Center(
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+      child: SizedBox(
+        width: 160.0,
+        height: 100.0,
+        child: ListTile(
+          title: Text(text),
+          contentPadding: EdgeInsets.fromLTRB(0.0, 20.0, 20.0, 0.0),
+          onTap: () async {
+            DateTime? date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1990),
+                lastDate: DateTime(3000)
+            );
+          },
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(backgroundImage: riconimage(imageName)),
+          ),
+          // trailing: Icon(Icons.navigate_next),
+          iconColor: Colors.white,
+          textColor: Colors.white,
+          tileColor: Colors.black,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget rapp(BuildContext context,String text,String imageName) {
+  return Center(
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+      child: SizedBox(
+        width: 160.0,
+        height: 100.0,
+        child: ListTile(
+          title: Text(text),
+          contentPadding: EdgeInsets.fromLTRB(0.0, 20.0, 30.0, 0.0),
+          onTap: () {
+            print("Hello World");
+          },
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(backgroundImage: riconimage(imageName)),
+          ),
+          // trailing: Icon(Icons.navigate_next),
+          iconColor: Colors.white,
+          textColor: Colors.white,
+          tileColor: Colors.black,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40.0)),
+        ),
+      ),
+    ),
+  );
+}
+
 Widget textfield({
     required String hintText,
     required IconData icon,
